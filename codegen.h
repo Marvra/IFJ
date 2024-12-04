c00a99da180be37d7d9794c05d96853d3fed28bc
Jaroslav Doktor <jaroslavdoktor@seznam.cz>
2024-12-04 17:19:25 +0100

    commenty

--- codegen.h
+++ codegen.h
@@ -17,10 +17,22 @@
 void createHeader();
 void createMain();
 void createFunction(char *function_id);
+void createVariable(char* var_id);
+void CreateNonVariableParams(int param);
+char* concat(const char *s1, const char *s2);
+char* removeFirstAndLast(const char* str);
+char* WriteString(ASTNode* node);
+void CreateArguments(ASTNode *node);
+void CreateParams(ASTNode *node);
 void createFunctionCall(char* function_id);
+void CreateExpression(ASTNode *node);
+void CreateReturn(ASTNode *node);
+void RelOperator(ASTNode *node, int cond);
+void RelOperatorWhile(ASTNode *node, int cond);
+void StartIfElse(ASTNode *node, int cond);
+void CreateWhile(ASTNode *node, int cond);
 void TraverseASTCodeGen(ASTNode *node);
 void CodeGen(ASTNode *node);
-char* WriteString(ASTNode* node);
 
 #endif
 
